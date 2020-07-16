c29b2355a6a2b9fe07fae7706592102a05729383
kycarr <kaylacar@usc.edu>
2020-07-16 12:51:44 -0700

    Loads session for lesson id in query param ?lesson=id (#3)
    * Loads session for lesson id in query param ?lesson=id
    
    * Test sending a message to lesson q2
    
    Fix q2-p2.json
    
    * Merge changes and comment out chat test temporarily
    
    Co-authored-by: kcarr <kcarr@ict.usc.edu>

---
+++ client/cypress/integration/chat.spec.js
@@ -0,0 +1,42 @@
+describe("The Home Page", () => {
+  it("shows closing messages with awesome answer", () => {
+    cy.server();
+    cy.viewport(660, 1000);
+    cy.visit("/?lesson=q1"); // change URL to match your dev URLs
+
+    //Part 1
+    cy.fixture("q1-p1.json").then((desiredServerResponse) => {
+      cy.route("POST", "**/dialog/q1", desiredServerResponse);
+
+      cy.get("#chat-msg-0").contains("Welcome to OpenTutor");
+      cy.get("#chat-msg-1").contains(
+        desiredServerResponse.response[0].data.text
+      );
+      cy.get("#chat-msg-2").contains(
+        desiredServerResponse.response[1].data.text
+      );
+    });
+
+    //Part 2
+    // cy.fixture("q1-p2.json").then((desiredServerResponse) => {
+    //   cy.route("POST", "**/dialog/q1/session", desiredServerResponse);
+
+    //   const reply1 =
+    //     "Peer pressure can cause you to allow inappropriate behavior. If you correct someone's behavior, you may get them in trouble or it may be harder to work with them. Enforcing the rules can make you unpopular.";
+
+    //   cy.get("#outlined-multiline-static").type(reply1);
+    //   cy.get("#submit-button").click();
+
+    //   cy.get("#chat-msg-3").contains(reply1);
+    //   cy.get("#chat-msg-4").contains(
+    //     desiredServerResponse.response[0].data.text
+    //   );
+    //   cy.get("#chat-msg-5").contains(
+    //     desiredServerResponse.response[1].data.text
+    //   );
+    //   cy.get("#chat-msg-6").contains(
+    //     desiredServerResponse.response[2].data.text
+    //   );
+    // });
+  });
+});
--- client/src/api.ts
+++ client/src/api.ts
@@ -3,15 +3,16 @@ import axios from "axios";
 const DIALOG_ENDPOINT = process.env.DIALOG_ENDPOINT || "/dialog/";
 
 // put dialog api calls here
-export async function createSession() {
-  return await axios.post(`${DIALOG_ENDPOINT}/q1`, {});
+export async function createSession(lesson: string) {
+  return await axios.post(`${DIALOG_ENDPOINT}/${lesson}`, {});
 }
 
 export async function continueSession(props: {
+  lesson: string;
   session: any;
   outboundChat: any;
 }) {
-  return await axios.post(`${DIALOG_ENDPOINT}/q1/session`, {
+  return await axios.post(`${DIALOG_ENDPOINT}/${props.lesson}/session`, {
     sessionInfo: props.session,
     message: props.outboundChat,
   });
