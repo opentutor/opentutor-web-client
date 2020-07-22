6ea755096242a0d5acce1fbbd4d358717a48d86a
StapleMink <daniel.budziwojski@gmail.com>
2020-07-21 17:08:10 -0700

    Update progress.spec.js

--- client/cypress/integration/progress.spec.js
+++ client/cypress/integration/progress.spec.js
@@ -1,5 +1,5 @@
 describe("Target Indicators Work", () => {
-    it("shows no progress on all three targets in lesson 1", () => {
+    it("shows no progress on all three targets at start of lesson 1", () => {
       cy.server();
       cy.viewport(660, 1000);
       cy.visit("/?lesson=q1"); // change URL to match your dev URLs
@@ -12,71 +12,87 @@ describe("Target Indicators Work", () => {
       });
     });
   
-    it("sends message and gets response for lesson q1", () => {
+    it("shows some progress on two targets during lesson 1", () => {
       cy.server();
       cy.viewport(660, 1000);
       cy.visit("/?lesson=q1"); // change URL to match your dev URLs
-      cy.route("POST", "**/dialog/q1", "fixture:q1-p1.json");
+      cy.route("POST", "**/dialog/q1", "fixture:q1-2-p2.json");
   
-      cy.fixture("q1-1-p2.json").then((desiredServerResponse) => {
+      cy.fixture("q1-2-p2.json").then((desiredServerResponse) => {
         cy.route("POST", "**/dialog/q1/session", desiredServerResponse);
-        const reply =
-          "Peer pressure can cause you to allow inappropriate behavior. If you correct someone's behavior, you may get them in trouble or it may be harder to work with them. Enforcing the rules can make you unpopular.";
+        const reply = "Peer pressure";
   
         cy.get("#outlined-multiline-static").type(reply);
         cy.get("#submit-button").click();
   
-        cy.get("#chat-msg-3").contains(reply);
-        cy.get("#chat-msg-4").contains(
-          desiredServerResponse.response[0].data.text
-        );
-        cy.get("#chat-msg-5").contains(
-          desiredServerResponse.response[1].data.text
-        );
-        cy.get("#chat-msg-6").contains(
-          desiredServerResponse.response[2].data.text
-        );
+        cy.get(`#target-0-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[0].score).toFixed()}`);
+        cy.get(`#target-1-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[1].score).toFixed()}`);
+        cy.get(`#target-2-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[2].score).toFixed()}`);
       });
     });
-  
-    it("starts session for lesson q2", () => {
-      cy.server();
-      cy.viewport(660, 1000);
-      cy.visit("/?lesson=q2"); // change URL to match your dev URLs
-  
-      cy.fixture("q2-1-p1.json").then((desiredServerResponse) => {
-        cy.route("POST", "**/dialog/q2", desiredServerResponse);
-        cy.get("#chat-msg-0").contains("Welcome to OpenTutor");
-        cy.get("#chat-msg-1").contains(
-          desiredServerResponse.response[0].data.text
-        );
-        cy.get("#chat-msg-2").contains(
-          desiredServerResponse.response[1].data.text
-        );
+
+    it("shows full progress on three targets with perfect response in lesson 1", () => {
+        cy.server();
+        cy.viewport(660, 1000);
+        cy.visit("/?lesson=q1"); // change URL to match your dev URLs
+        cy.route("POST", "**/dialog/q1", "fixture:q1-1-p2.json");
+    
+        cy.fixture("q1-1-p2.json").then((desiredServerResponse) => {
+          cy.route("POST", "**/dialog/q1/session", desiredServerResponse);
+          const reply = "Peer pressure can cause you to allow inappropriate behavior. If you correct someone's behavior, you may get them in trouble or it may be harder to work with them. Enforcing the rules can make you unpopular.";
+    
+          cy.get("#outlined-multiline-static").type(reply);
+          cy.get("#submit-button").click();
+    
+          cy.get(`#target-0-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[0].score).toFixed()}`);
+          cy.get(`#target-1-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[1].score).toFixed()}`);
+          cy.get(`#target-2-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[2].score).toFixed()}`);
+        });
       });
-    });
-  
-    it("sends message and gets response for lesson q2", () => {
-      cy.server();
-      cy.viewport(660, 1000);
-      cy.visit("/?lesson=q2"); // change URL to match your dev URLs
-      cy.route("POST", "**/dialog/q2", "fixture:q2-p1.json");
-  
-      cy.fixture("q2-1-p2.json").then((desiredServerResponse) => {
-        cy.route("POST", "**/dialog/q2/session", desiredServerResponse);
-        const reply = "Current flows in the same direction as the arrow";
-  
-        cy.get("#outlined-multiline-static").type(reply);
-        cy.get("#submit-button").click();
-  
-        cy.get("#chat-msg-3").contains(reply);
-        cy.get("#chat-msg-4").contains(
-          desiredServerResponse.response[0].data.text
-        );
-        cy.get("#chat-msg-5").contains(
-          desiredServerResponse.response[1].data.text
-        );
+      //Q2
+      it("shows no progress on all three targets at start of lesson 2", () => {
+        cy.server();
+        cy.viewport(660, 1000);
+        cy.visit("/?lesson=q2"); // change URL to match your dev URLs
+    
+        cy.fixture("q2-1-p1.json").then((desiredServerResponse) => {
+          cy.route("POST", "**/dialog/q2", desiredServerResponse);
+          cy.get(`#target-0-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[0].score).toFixed()}`);
+        });
       });
-    });
+    
+      it("shows some progress on two targets during lesson 2", () => {
+        cy.server();
+        cy.viewport(660, 1000);
+        cy.visit("/?lesson=q2"); // change URL to match your dev URLs
+        cy.route("POST", "**/dialog/q2", "fixture:q2-2-p2.json");
+    
+        cy.fixture("q2-2-p2.json").then((desiredServerResponse) => {
+          cy.route("POST", "**/dialog/q2/session", desiredServerResponse);
+          const reply = "Peer pressure";
+    
+          cy.get("#outlined-multiline-static").type(reply);
+          cy.get("#submit-button").click();
+    
+          cy.get(`#target-0-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[0].score).toFixed()}`);
+        });
+      });
+  
+      it("shows full progress on three targets with perfect response in lesson 2", () => {
+          cy.server();
+          cy.viewport(660, 1000);
+          cy.visit("/?lesson=q2"); // change URL to match your dev URLs
+          cy.route("POST", "**/dialog/q2", "fixture:q2-1-p2.json");
+      
+          cy.fixture("q2-1-p2.json").then((desiredServerResponse) => {
+            cy.route("POST", "**/dialog/q2/session", desiredServerResponse);
+            const reply = "Peer pressure can cause you to allow inappropriate behavior. If you correct someone's behavior, you may get them in trouble or it may be harder to work with them. Enforcing the rules can make you unpopular.";
+      
+            cy.get("#outlined-multiline-static").type(reply);
+            cy.get("#submit-button").click();
+      
+            cy.get(`#target-0-${Number(desiredServerResponse.sessionInfo.dialogState.expectationData[0].score).toFixed()}`);
+          });
+        });
   });
   
