bb5829823c87f47bb36697799af23f02ef0d39c1
larry kirschner <larrykirschner@gmail.com>
2020-08-12 12:21:26 -0700

    fixes: cypress tests should always mock all routes before calling cy.visit

--- client/cypress/integration/input.spec.js
+++ client/cypress/integration/input.spec.js
@@ -1,55 +1,55 @@
-import { visitOnMobile } from "../support/functions";
+import { cySetup } from "../support/functions";
 
 describe("Input field", () => {
   it("disables send button when no input", () => {
-    visitOnMobile(cy, "/?lesson=q1"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q1", "fixture:q1-1-p1.json");
-
+    cy.visit("/?lesson=q1");
     cy.get("#outlined-multiline-static").should("not.be.disabled");
     cy.get("#submit-button").should("be.disabled");
   });
 
   it("disables send button when session finished", () => {
-    visitOnMobile(cy, "/?lesson=q1"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q1", "fixture:q1-1-p1.json");
     cy.route("POST", "**/dialog/q1/session", "fixture:q1-1-p2.json");
-
-    const reply1 =
-      "Peer pressure can cause you to allow inappropriate behavior. If you correct someone's behavior, you may get them in trouble or it may be harder to work with them. Enforcing the rules can make you unpopular.";
-    cy.get("#outlined-multiline-static").type(reply1);
+    cy.visit("/?lesson=q1");
+    cy.get("#outlined-multiline-static").type(
+      "Peer pressure can cause you to allow inappropriate behavior. If you correct someone's behavior, you may get them in trouble or it may be harder to work with them. Enforcing the rules can make you unpopular."
+    );
     cy.get("#submit-button").click();
     cy.get("#submit-button").should("be.disabled");
     cy.get("#outlined-multiline-static").should("be.disabled");
   });
 
   it("enables send button when input and session not finished", () => {
-    visitOnMobile(cy, "/?lesson=q1"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q1", "fixture:q1-1-p1.json");
-
+    cy.visit("/?lesson=q1");
     cy.get("#outlined-multiline-static").type("hi");
     cy.get("#outlined-multiline-static").should("not.be.disabled");
     cy.get("#submit-button").should("not.be.disabled");
   });
 
   it("can send input with button", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
     cy.route("POST", "**/dialog/q2/session", "fixture:q2-1-p2.json");
-
-    const reply = "Current flows in the same direction as the arrow";
-    cy.get("#outlined-multiline-static").type(reply);
+    cy.visit("/?lesson=q2");
+    const userInput = "Current flows in the same direction as the arrow";
+    cy.get("#outlined-multiline-static").type(userInput);
     cy.get("#submit-button").click();
-    cy.get("#chat-msg-3").contains(reply);
+    cy.get("#chat-msg-3").contains(userInput);
   });
 
   it("can send input with enter", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
     cy.route("POST", "**/dialog/q2/session", "fixture:q2-1-p2.json");
-
-    const reply = "Current flows in the same direction as the arrow";
-    cy.get("#outlined-multiline-static").type(reply);
+    cy.visit("/?lesson=q2");
+    const userInput = "Current flows in the same direction as the arrow";
+    cy.get("#outlined-multiline-static").type(userInput);
     cy.get("#outlined-multiline-static").type("{enter}");
-    cy.get("#chat-msg-3").contains(reply);
+    cy.get("#chat-msg-3").contains(userInput);
   });
 });
--- client/cypress/integration/summary.spec.js
+++ client/cypress/integration/summary.spec.js
@@ -1,38 +1,42 @@
-import { visitOnMobile } from "../support/functions";
+import { cySetup } from "../support/functions";
 
 describe("Expectation summary pop-up", () => {
   it("opens with View Summary button", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
-
+    cy.visit("/?lesson=q2");
     cy.get("#view-summary-btn").click();
     cy.get("#summary-popup");
   });
 
   [
     {
-      lesson: 'q1',
-      fixture: 'q1-1-p1.json',
-      expectedExpectations: 3
+      lesson: "q1",
+      fixture: "q1-1-p1.json",
+      expectedExpectations: 3,
     },
     {
-      lesson: 'q2',
-      fixture: 'q2-1-p1.json',
-      expectedExpectations: 1
-    }
-  ].forEach(x => {
+      lesson: "q2",
+      fixture: "q2-1-p1.json",
+      expectedExpectations: 1,
+    },
+  ].forEach((x) => {
     it(`shows an icon for each expectation for lesson with ${x.expectedExpectations}`, () => {
-      visitOnMobile(cy, `/?lesson=${x.lesson}`); // change URL to match your dev URLs
+      cySetup(cy);
       cy.route("POST", `**/dialog/${x.lesson}`, `fixture:${x.fixture}`);
+      cy.visit(`/?lesson=${x.lesson}`);
       cy.get("#view-summary-btn").click();
       cy.get("#summary-popup");
-      cy.get("#summary-targets").children().should("have.length", x.expectedExpectations);
+      cy.get("#summary-targets")
+        .children()
+        .should("have.length", x.expectedExpectations);
     });
-  })
+  });
 
   it("displays on tap any expectation-progress indicator", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
+    cy.visit("/?lesson=q2");
     cy.fixture("q2-1-p1.json").then((desiredServerResponse) => {
       cy.get(
         `#target-0-${Number(
@@ -45,21 +49,21 @@ describe("Expectation summary pop-up", () => {
   });
 
   it("displays at when lesson is completed", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
-
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
     cy.route("POST", "**/dialog/q2/session", "fixture:q2-1-p2.json");
-
-    const reply = "Current flows in the same direction as the arrow";
-    cy.get("#outlined-multiline-static").type(reply);
+    cy.visit("/?lesson=q2");
+    cy.get("#outlined-multiline-static").type(
+      "Current flows in the same direction as the arrow"
+    );
     cy.get("#submit-button").click();
     cy.get("#summary-popup");
   });
 
   it("hides text for expectations that have not been completed", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
-
+    cy.visit("/?lesson=q2");
     cy.get("#view-summary-btn").click();
     cy.get("#summary-popup");
     cy.get("#summary-targets").children().should("have.length", 1);
@@ -67,9 +71,9 @@ describe("Expectation summary pop-up", () => {
   });
 
   it("shows no progress for expectations that have not been completed", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
-
+    cy.visit("/?lesson=q2");
     cy.get("#view-summary-btn").click();
     cy.get("#summary-popup");
     cy.get("#summary-targets").children().should("have.length", 1);
@@ -77,12 +81,13 @@ describe("Expectation summary pop-up", () => {
   });
 
   it("shows text for expectations that have been completed", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
     cy.route("POST", "**/dialog/q2/session", "fixture:q2-1-p2.json");
-
-    const reply = "Current flows in the same direction as the arrow";
-    cy.get("#outlined-multiline-static").type(reply);
+    cy.visit("/?lesson=q2");
+    cy.get("#outlined-multiline-static").type(
+      "Current flows in the same direction as the arrow"
+    );
     cy.get("#submit-button").click();
     cy.get("#summary-popup");
     cy.get("#summary-targets").children().should("have.length", 1);
@@ -92,12 +97,13 @@ describe("Expectation summary pop-up", () => {
   });
 
   it("shows progress for expectations that have been completed", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
+    cySetup(cy);
     cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
     cy.route("POST", "**/dialog/q2/session", "fixture:q2-1-p2.json");
-
-    const reply = "Current flows in the same direction as the arrow";
-    cy.get("#outlined-multiline-static").type(reply);
+    cy.visit("/?lesson=q2");
+    cy.get("#outlined-multiline-static").type(
+      "Current flows in the same direction as the arrow"
+    );
     cy.get("#submit-button").click();
     cy.get("#summary-popup");
     cy.get("#summary-targets").children().should("have.length", 1);
