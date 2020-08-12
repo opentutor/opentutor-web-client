2df965ccc3549d401df80feb296bbe273852213c
larry kirschner <larrykirschner@gmail.com>
2020-08-12 09:20:42 -0700

    improvement: parameterize exp-count tests

--- client/cypress/integration/expectations.spec.js
+++ client/cypress/integration/expectations.spec.js
@@ -1,17 +1,22 @@
 import { visitOnMobile } from "../support/functions";
 
 describe("Expectation indicators", () => {
-  it("shows icons for each expectation for lesson q1", () => {
-    visitOnMobile(cy, "/?lesson=q1"); // change URL to match your dev URLs
-    cy.route("POST", "**/dialog/q1", "fixture:q1-1-p1.json");
-
-    cy.get("#targets").children().should("have.length", 3);
-  });
-
-  it("shows icons for each expectation for lesson q2", () => {
-    visitOnMobile(cy, "/?lesson=q2"); // change URL to match your dev URLs
-    cy.route("POST", "**/dialog/q2", "fixture:q2-1-p1.json");
-
-    cy.get("#targets").children().should("have.length", 1);
+  [
+    {
+      lesson: "q1",
+      fixture: "q1-1-p1.json",
+      expectedExpCount: 3,
+    },
+    {
+      lesson: "q2",
+      fixture: "q2-1-p1.json",
+      expectedExpCount: 1,
+    },
+  ].forEach((x) => {
+    it(`shows an icon for each expectation for a lesson with ${x.expectedExpCount}`, () => {
+      visitOnMobile(cy, `/?lesson=${x.lesson}`); // change URL to match your dev URLs
+      cy.route("POST", `**/dialog/${x.lesson}`, `fixture:${x.fixture}`);
+      cy.get("#targets").children().should("have.length", x.expectedExpCount);
+    });
   });
 });
