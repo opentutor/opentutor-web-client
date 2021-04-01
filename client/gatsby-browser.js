8db554308055b8bf6e52058e8efea4263a19dad4
larry kirschner <larrykirschner@gmail.com>
2021-04-01 12:43:37 -0700

    fixes lint errors

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -9,6 +9,7 @@ const theme = createMuiTheme({
   },
 });
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const wrapRootElement = ({ element }) => (
   <MuiThemeProvider theme={theme}>{element}</MuiThemeProvider>
 );
