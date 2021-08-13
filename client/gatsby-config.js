d69d416233c5cdf047860759146f0d252f049dd2
Jim Alvarez <jimmyaugust0506@gmail.com>
2021-08-13 11:53:27 -0500

    Docker builds and deploys locally on M1 machines.

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -5,18 +5,7 @@ module.exports = {
     description: ``,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: "src/images/icon.png",
-      },
-    },
+    
     {
       resolve: `gatsby-plugin-env-variables`,
       options: {
