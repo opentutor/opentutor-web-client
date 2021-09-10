95aaa46d2e8d3eedf3d48db9f7ce8fc64b8790ec
Daniel Budziwojski <daniel.budziwojski@gmail.com>
2021-09-09 18:11:52 -0700

    Factor out SurveySays into own component

--- client/src/components/SurveySays.tsx
+++ client/src/components/SurveySays.tsx
@@ -4,3 +4,86 @@ Permission to use, copy, modify, and distribute this software and its documentat
 
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { Target } from "types";
+import clsx from "clsx";
+import withLocation from "wrap-with-location";
+
+const useStyles = makeStyles((theme) => ({
+  centerLock: {
+    position: "absolute",
+    top: "50%",
+    left: "calc(50% + 0px)",
+    transform: "translate(-50%, -50%)",
+  },
+  censored: {
+    borderRadius: 10,
+    background: theme.palette.primary.main,
+    color: "white",
+    padding: 10,
+    height: 10,
+    width: "calc(100% - 20px)",
+    position: "relative",
+    borderColor: "orange",
+    borderWidth: 2,
+    borderStyle: "solid",
+  },
+  survey: {
+    padding: theme.spacing(2),
+    maxWidth: 500,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  bodyRoot: {},
+  bodyNoMedia: {
+    height: "calc(30% - 113px)"
+  },
+  bodyMedia: {
+    height: "calc(60% - 113px)"
+  },
+}));
+
+const SurveySays = (props: {
+  search: { lesson: string };
+  hasMedia: boolean;
+  targets: Target[];
+}): JSX.Element => {
+  const styles = useStyles();
+
+  return (
+    <>
+      <div className={clsx({
+            [styles.bodyRoot]: true,
+            [styles.bodyNoMedia]: !props.hasMedia,
+            [styles.bodyMedia]: props.hasMedia,
+          })}>
+        <div className={styles.survey}>
+          <Grid container spacing={2}>
+            {props.targets.map((target, idx) => {
+              return (
+                <Grid container item spacing={2} key={idx}>
+                  <Grid item xs={12}>
+                    <div className={styles.censored}>
+                      <Typography
+                        className={styles.centerLock}
+                        variant={!target.achieved ? "h6" : "caption"}
+                        style={{ width: "100%" }}
+                      >
+                        {target.achieved ? target.text : idx + 1}
+                      </Typography>
+                    </div>
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default withLocation(SurveySays);
