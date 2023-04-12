8fee89576c24eaf07beb5e44b46c34a7c81888db
dan100110 <daniel.auerbach@gmail.com>
2023-04-12 15:12:39 -0700

    development build working

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -7,6 +7,10 @@ exports.onCreateWebpackConfig = ({ actions }) => {
         "@": path.resolve(__dirname, "src"),
       },
       modules: [path.resolve(__dirname, "src"), "node_modules"],
+      fallback: {
+        stream: require.resolve('stream-browserify'),
+        util: require.resolve("util"),
+      }
     },
   });
 };
