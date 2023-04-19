28f8bacee07b1116e42d37d68d4fe4aafceb17f4
dan100110 <daniel.auerbach@gmail.com>
2023-04-19 13:55:21 -0700

    should build now

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,16 +1,29 @@
 const path = require("path");
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  config = {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
       },
       modules: [path.resolve(__dirname, "src"), "node_modules"],
       fallback: {
-        stream: require.resolve('stream-browserify'),
+        stream: require.resolve("stream-browserify"),
         util: require.resolve("util"),
-      }
+      },
     },
-  });
+  };
+  // Replace this module with a dummy module during SSR since it expects browser to always be available https://www.gatsbyjs.com/docs/debugging-html-builds/#fixing-third-party-modules
+  if (stage === "build-html" || stage === "develop-html") {
+    config["module"] = {
+      rules: [
+        {
+          test: /xapi/,
+          use: loaders.null(),
+        },
+      ],
+    };
+  }
+
+  actions.setWebpackConfig(config);
 };
